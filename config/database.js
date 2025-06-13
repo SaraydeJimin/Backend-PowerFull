e2de724d6b749df6970a41a5fb78a020190007cc
Julieth Florez <kslopez89@soy.sena.edu.co>
2025-06-12 21:59:05 -0500

    ignorar node_modules y .env

--- config/database.js
+++ config/database.js
@@ -4,24 +4,18 @@ const { Pool } = require("pg");
 const connectDB = async () => {
   try {
     const pool = new Pool({
-      host: process.env.BDSENA_HOST,
-      user: process.env.BDSENA_USER,
-      password: process.env.BDSENA_PASSWORD,
-      database: process.env.BDSENA_DATABASE,
-      port: 5432, // Puerto por defecto de PostgreSQL
       connectionString: process.env.DATABASE_URL,
       ssl: {
-        rejectUnauthorized: false, // Si usas Render o alguna plataforma con SSL
+        rejectUnauthorized: false, // Necesario para Render
       },
     });
 
-    // Verifica la conexión
     await pool.query("SELECT NOW()");
     console.log("Conexión exitosa a la base de datos PostgreSQL.");
 
     return pool;
   } catch (error) {
-    console.error("Error conectando a la base de datos PostgreSQL:", error.message, error.stack);
+    console.error("Error conectando a la base de datos PostgreSQL:", error.message);
     throw error;
   }
 };
